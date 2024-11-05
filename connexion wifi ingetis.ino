557986abc3dabbe0a87890d8ff4d38d83eef51c6
Nexinor <nextinor@gmail.com>
2024-11-05 15:45:19 +0100

    commit code source levan

---
+++ connexion wifi ingetis.ino
@@ -0,0 +1,40 @@
+#include <ESP8266WiFi.h>
+
+const char* ssid = "INGETIS";
+const char* password = "1Gti$C@mpu$";
+
+WiFiServer server(80);
+
+
+void setup() {
+  Serial.begin(115200);
+  delay(100);
+
+  Serial.println();
+  Serial.print("Connexion au réseau ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("Connexion au WiFi établie");
+  Serial.print("Addresse IP: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+void loop() {
+ if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("Connexion WiFi active.");
+  } else {
+    Serial.println("Connexion WiFi perdue");
+  }
+
+
+  delay(1000);
+}
